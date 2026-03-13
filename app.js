12fec591ba7153334a8d487143bdcbe5d9c41feb
thaheer <126898171+thaheernitturi@users.noreply.github.com>
2026-03-13 21:08:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ const express = require("express");
 
 const app = express();
 
-// Custom Middleware
-const addUserMiddleware = (req, res, next) => {
-  req.user = "Guest";   // adding property to request
-  next();               // pass control to next middleware/route
-};
+// GET /orders
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders.");
+});
 
-// Route using middleware
-app.get("/welcome", addUserMiddleware, (req, res) => {
-  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+// POST /orders
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+
+// GET /users
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+
+// POST /users
+app.post("/users", (req, res) => {
+  res.send("A new user has been added.");
 });
 
 // Start server
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server is running on http://localhost:3000");
 });
