a7dd722754c4527bb5d2978bc81f8813145924b1
thaheer <126898171+thaheernitturi@users.noreply.github.com>
2026-03-13 21:10:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ const express = require("express");
 
 const app = express();
 
-// GET /orders
-app.get("/orders", (req, res) => {
-  res.send("Here is the list of all orders.");
+// Products routes
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
 });
 
-// POST /orders
-app.post("/orders", (req, res) => {
-  res.send("A new order has been created.");
+app.post("/products", (req, res) => {
+  res.send("A new product has been added.");
 });
 
-// GET /users
-app.get("/users", (req, res) => {
-  res.send("Here is the list of all users.");
+// Categories routes
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
 });
 
-// POST /users
-app.post("/users", (req, res) => {
-  res.send("A new user has been added.");
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created.");
+});
+
+// Wildcard route for undefined routes
+app.use("*", (req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(4000, () => {
+  console.log("Server running on http://localhost:4000");
 });
